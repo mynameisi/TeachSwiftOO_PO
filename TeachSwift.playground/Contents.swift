35994caf181eabea3718b52db30ad8511f36404a
mynameisi <test@nowhere.com>
2015-12-30 17:30:21 +0800

    首次commit
    只有comment还没有代码

---
+++ TeachSwift.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//上课讲解代码
+//面向过程
+//定义一个方法 吃()
+//定义一个方法 睡()
+//定义一个方法 讲课()
+//打印张三吃饭，张三睡觉，张三教JAVA
+
+
+//面向对象
+//定义一个People类
+//属性：姓名，爱吃鱼，年龄，身高
+//方法：
+//eat(food?) 如果传入food，就打印在吃food，否则要么吃鱼，要么吃虾
+//sleep() 根据身高判断睡长的床还是短的床
+
+
+//定义一个老师类，继承People类
+//方法：teach(class)
+
+//打印张三吃饭，张三睡觉，张三教JAVA
+
